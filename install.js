1f4513f88686002eacb1ebdb52d976baa3002107
Vince Bickers <vince.bickers@gmail.com>
2021-05-28 11:33:30 +0100

    Update install.js

--- install.js
+++ install.js
@@ -49,7 +49,7 @@ PREFIX="/usr/local" sudo make install
 
 # jenkins
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > etc/apt/sources.list.d/jenkins.list'
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt install jenkins
 
