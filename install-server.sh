ae905202b052229967fdefd734da17d4948fa1f5
Vince Bickers <vince.bickers@gmail.com>
2021-06-01 07:49:29 +0100

    Create install-server.sh

---
+++ install-server.sh
@@ -0,0 +1,33 @@
+# apt packages
+sudo apt install openjdk-8-jdk
+sudo apt install maven
+sudo apt install xmlstarlet
+sudo apt install zip
+sudo apt install build-essential
+
+# nginx
+sudo apt install nginx
+sudo cat /certs/clientcert.cer /certs/cacert.cer | sudo tee /certs/ssl-bundle.cer
+
+echo -n "Enter the public URL of this server and press [ENTER]"
+read SERVER_NAME
+
+sudo tee /etc/nginx/sites-available/default << EOF
+server {
+    listen 443 ssl http2;
+    server_name $SERVER_NAME;
+    
+    location / {
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_pass http://127.0.0.1:8080;
+    }
+    
+    ssl_certificate /certs/ssl-bundle.cer;
+    ssl_certificate_key /certs/clientcert.key;
+} 
+EOF
+sudo nginx -t
+sudo systemctl restart nginx
