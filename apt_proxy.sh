9819afa99a343b8c917e9567d88bd4e9976df20e
Vince Bickers <vince.bickers@gmail.com>
2022-04-25 07:27:28 +0100

    Delete apt_proxy.sh

--- apt_proxy.sh
+++
@@ -1,3 +0,0 @@
-sudo su -
-echo 'Acquire::http::Proxy ' "http://10.40.126.7:3128/";' > /etc/apt/apt.conf.d/proxy.conf
-echo 'Acquire::https::Proxy ' "http://10.40.126.7:3128/";' >> /etc/apt/apt.conf.d/proxy.conf
